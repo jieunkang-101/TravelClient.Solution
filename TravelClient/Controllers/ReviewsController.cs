7bff541a574767803e40c3bffc3785e86e4ab7f3
Jieun Kang <jieunkang101@gmail.com>
2020-04-01 13:10:55 -0700

    add ApiHelper.cs and ReviewsController.cs and add GetReviews() in Reveiw.cs

---
+++ TravelClient/Controllers/ReviewsController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TravelClient.Models;
+
+namespace TravelClient.Controllers
+{
+  public class ReviewsController : Controller
+  {
+    public IActionResult Index()
+    {
+      var allReviews = Review.GetReviews();
+      return View(allReviews);
+    }
+
+
+  
+  }
+}  
--- TravelClient/Models/Review.cs
+++ TravelClient/Models/Review.cs
@@ -3,7 +3,7 @@ using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace CretaceousClient.Models
+namespace TravelClient.Models
 {
   public class Review
   {
@@ -14,6 +14,18 @@ namespace CretaceousClient.Models
     public string Landmark { get; set; }
     public string Description { get; set; }
     public int Rating { get; set; }
+
+
+    public static List<Review> GetReviews()
+    {
+      var apiCallTask = ApiHelper.GetAll();
+      var result = apiCallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result); //The API call results in a JSON array
+      List<Review> reviewList = JsonConvert.DeserializeObject<List<Review>>(jsonResponse.ToString());
+
+      return reviewList;
+    }
     
   }
 }    
