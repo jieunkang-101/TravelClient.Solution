9edbc4529c3bb4ba110a5204c1bb6e4d5e636190
Jieun Kang <jieunkang101@gmail.com>
2020-04-02 16:08:06 -0700

    add Date property in Review class and display datet and time in Review details pase

--- TravelClient/Models/Review.cs
+++ TravelClient/Models/Review.cs
@@ -14,6 +14,7 @@ namespace TravelClient.Models
     public string Landmark { get; set; }
     public string Description { get; set; }
     public int Rating { get; set; }
+    public DateTime Date { get; set; }
 
 
     public static List<Review> GetReviews()
